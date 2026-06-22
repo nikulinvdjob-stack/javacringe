91f7aa2f6e88b57959f91b040365f25c868d5af0
unknown <you@example.com>
2026-06-22 16:22:19 +0300

    add logic to Task class

--- HWwithGradle/src/main/java/ru/alfabank/hw/hw10/Task.java
+++ HWwithGradle/src/main/java/ru/alfabank/hw/hw10/Task.java
@@ -1,4 +1,24 @@
 package ru.alfabank.hw.hw10;
 
 public class Task {
+
+    private String name;
+    private boolean isDone = false;
+
+    public void markTaskAsDone() {
+        isDone = true;
+    }
+
+    public boolean getTaskStatus() {
+        return isDone;
+    }
+
+    public String getTaskName() {
+        return name;
+    }
+
+    public String getTaskInfo() {
+        return isDone ? "[] Проверить авторизацию" : "[] Проверить регистрацию";
+    }
+
 }
