51c1846d6fc82e17c4ce2c140af9da37bcf843e9
unknown <you@example.com>
2026-07-10 16:52:57 +0300

    add hw 13

--- src/main/java/ru/alfabank/hw/hw13/MissionReport.java
+++ src/main/java/ru/alfabank/hw/hw13/MissionReport.java
@@ -1,4 +1,37 @@
 package ru.alfabank.hw.hw13;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class MissionReport {
+    private String missionName;
+    private List<Alien> captureAliens = new ArrayList<>();
+    int squadSize;
+
+    public MissionReport(String missionName, List<Alien> captureAliens, int squadSize) {
+        this.missionName = missionName;
+        this.captureAliens = captureAliens;
+        this.squadSize = squadSize;
+    }
+
+    @Override
+    public String toString() {
+        return "MissionReport{" +
+                "Миссия: '" + missionName + '\'' +
+                ", Поймано чекушек: " + captureAliens.size() +
+                ", Размер отряда: " + squadSize +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MissionReport that)) return false;
+        return squadSize == that.squadSize && Objects.equals(missionName, that.missionName) && Objects.equals(captureAliens, that.captureAliens);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(missionName, captureAliens, squadSize);
+    }
 }
--- src/main/java/ru/alfabank/hw/hw13/SquadManager.java
+++ src/main/java/ru/alfabank/hw/hw13/SquadManager.java
@@ -1,4 +1,104 @@
 package ru.alfabank.hw.hw13;
 
+import net.datafaker.Faker;
+
+import java.util.*;
+
 public class SquadManager {
+    static final Faker faker = new Faker();
+    static final Random random = new Random();
+
+    public String GetName() {
+        return (random.nextBoolean() ? "" : "Трус ") + faker.starWars().character();
+    }
+
+    public void demonstrateListCreations() {
+        List<String> mainSquad = new ArrayList<>();
+        for(int i = 0; i < 4; i++)
+            mainSquad.add(GetName());
+
+        List<String> supportSquad = Arrays.asList(
+                GetName(),
+                GetName(),
+                GetName(),
+                GetName());
+
+        List<String> eliteSquad = List.of(
+                GetName(),
+                GetName());
+
+        String str = "Отряд %s, %s - %s%n";
+        String squadType = "Основной";
+        String actionType = "Добавление";
+
+        try {
+            mainSquad.add(faker.starWars().character());
+            System.out.printf(str, squadType, actionType, "✅");
+        } catch (Exception e) {
+            System.out.printf(str, squadType, actionType,  "🚫: " + e.getClass().getSimpleName());
+        }
+
+        actionType = "Удаление";
+        try {
+            mainSquad.removeFirst();
+            System.out.printf(str, squadType, actionType,  "✅");
+        } catch (Exception e) {
+            System.out.printf(str, squadType, actionType,  "🚫: " + e.getClass().getSimpleName());
+        }
+
+        squadType = "Поддержки";
+        actionType = "Добавление";
+        try {
+            supportSquad.add(faker.starWars().character());
+            System.out.printf(str, squadType, actionType, "✅");
+        } catch (Exception e) {
+            System.out.printf(str, squadType, actionType, "🚫: " + e.getClass().getSimpleName());
+        }
+
+        actionType = "Удаление";
+        try {
+            supportSquad.removeFirst();
+            System.out.printf(str, squadType, actionType, "✅");
+        } catch (Exception e) {
+            System.out.printf(str, squadType, actionType, "🚫: " + e.getClass().getSimpleName());
+        }
+
+        squadType = "Элиты";
+        actionType = "Добавление";
+        try {
+            eliteSquad.add(faker.starWars().character());
+            System.out.printf(str, squadType, actionType, "✅");
+        } catch (Exception e) {
+            System.out.printf(str, squadType, actionType, "🚫: " + e.getClass().getSimpleName());
+        }
+
+        actionType = "Удаление";
+        try {
+            eliteSquad.removeFirst();
+            System.out.printf(str, squadType, actionType, "✅");
+        } catch (Exception e) {
+            System.out.printf(str, squadType, actionType, "🚫: " + e.getClass().getSimpleName());
+        }
+    }
+
+    public void filterOutCowards(List<String> squad) {
+        System.out.println("До фильтрации: " + squad);
+        Iterator<String> iterator = squad.iterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            if (name.startsWith("Трус")) {
+                iterator.remove();
+            }
+        }
+        System.out.println("После: " + squad);
+    }
+
+    public void filterOutCowardsRemoveIf(List<String> squad) {
+        System.out.println("До фильтрации: " + squad);
+        Iterator<String> iterator = squad.iterator();
+
+        squad.removeIf(name -> name.startsWith("Трус"));
+        System.out.println("После: " + squad);
+    }
 }
